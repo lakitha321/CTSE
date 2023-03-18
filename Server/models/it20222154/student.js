1435f46831be56e87d18e0814a1fa89f00fd8125
Sanduni Jayasinghe <sandunimaleesha316@gmail.com>
2023-03-18 12:05:39 +0530

    model created

---
+++ Server/models/it20222154/student.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema(
+  {
+    student_name: {
+      type: String,
+      required: true
+    },
+    parent_name: {
+      type: String,
+      required: true
+    },
+    student_phone: {
+      type: String,
+      required: true
+    },
+    parent_phone: {
+      type: String,
+      required: true
+    },
+    nic: {
+      type: String,
+      required: true
+    },
+    student_email: {
+      type: String,
+      required: true
+    },
+    parent_email: {
+      type: String,
+      required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+  }
+);
+
+const Student = mongoose.model('student', studentSchema);
+
+module.exports = Student;
