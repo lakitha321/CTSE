2d165d6feb121b1d1bbf6e8dcde7982721ceb4ba
PiniduInduwara <88415861+PiniduInduwara@users.noreply.github.com>
2023-03-18 15:54:42 +0530

    model created

--- Server/models/it20202286/notice.js
+++ Server/models/it20202286/notice.js
@@ -2,7 +2,30 @@ const mongoose = require('mongoose');
 
 const noticeSchema = new mongoose.Schema(
   {
-
+    date: {
+      type: String,
+      required: true
+    },
+    time: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    year: {
+      type: String,
+      required: true
+    },
+    class: {
+      type: String,
+      required: true
+    }
   }
 );
 
