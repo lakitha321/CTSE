e87864372ace458d3a12bcbee85e4280e33dca8c
PiniduInduwara <88415861+PiniduInduwara@users.noreply.github.com>
2023-03-24 10:10:53 +0530

    notices server

--- Client/screens/it20202286/start.js
+++ Client/screens/it20202286/start.js
@@ -1,28 +1,64 @@
-import React from "react";
-import {
-  StyleSheet,
-  View,
-  Text
-} from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
+export default function App() {
+  const [button1Pressed, setButton1Pressed] = useState(false);
+  const [button2Pressed, setButton2Pressed] = useState(false);
+  const [button3Pressed, setButton3Pressed] = useState(false);
 
-const App = () => {
+  const handleButton1Press = () => {
+    setButton1Pressed(!button1Pressed);
+  };
 
-  return(
-    <>
-    <View>
-      <Text style={styles.headertext}>PININDU</Text>
+  const handleButton2Press = () => {
+    setButton2Pressed(!button2Pressed);
+  };
+
+  const handleButton3Press = () => {
+    setButton3Pressed(!button3Pressed);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={[styles.button, button1Pressed && styles.buttonPressed]}
+        onPress={handleButton1Press}
+      >
+        <Text style={styles.buttonText}>Button 1</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, button2Pressed && styles.buttonPressed]}
+        onPress={handleButton2Press}
+      >
+        <Text style={styles.buttonText}>Button 2</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, button3Pressed && styles.buttonPressed]}
+        onPress={handleButton3Press}
+      >
+        <Text style={styles.buttonText}>Button 3</Text>
+      </TouchableOpacity>
     </View>
-    </>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  headertext: {
-    fontSize: 30,
-    letterSpacing: 0.25,
-    color: 'black',
-  }
-})
-
-export default App;
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+    margin: 10,
+  },
+  buttonPressed: {
+    backgroundColor: '#4CAF50',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
